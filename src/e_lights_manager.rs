57e896de948ee84b01afdd3cbdd9aec2f2e4561e
Siebencorgie <siebencorgie@googlemail.com>
2017-04-29 22:54:54 +0200

    added get_er, set_er, and creation

--- src/e_lights_manager.rs
+++ src/e_lights_manager.rs
@@ -25,3 +25,43 @@ impl LightOptions {
         LightOptions {max_lights: max_lights}
     }
 }
+
+
+impl LightManager {
+
+    //Create the light manager with its settings
+    pub fn new(max_lights: u32) -> Self {
+        let light_options = LightOptions::new(max_lights);
+
+        LightManager {  light_settings: light_options, directional_lights: HashMap::new(),
+                        spot_lights: HashMap::new(), point_lights: HashMap::new()}
+    }
+
+    //Add_er for the lights
+    pub fn add_directional_light(&mut self, name: &str, dir_light: e_light::Light_Directional) {
+        self.directional_lights.insert(String::from(name), dir_light);
+    }
+
+    pub fn add_spot_light(&mut self, name: &str, spot_light: e_light::Light_Spot) {
+        self.spot_lights.insert(String::from(name), spot_light);
+    }
+
+    pub fn add_point_light(&mut self, name: &str, point_light: e_light::Light_Point) {
+        self.point_lights.insert(String::from(name), point_light);
+    }
+
+
+    //Get_er
+    pub fn get_directional_light(&mut self, name: &str) ->Option<&mut e_light::Light_Directional> {
+        self.directional_lights.get_mut(&String::from(name))
+    }
+
+    pub fn get_spot_light(&mut self, name: &str) ->Option<&mut e_light::Light_Spot> {
+        self.spot_lights.get_mut(&String::from(name))
+    }
+
+    pub fn get_point_light(&mut self, name: &str) ->Option<&mut e_light::Light_Point> {
+        self.point_lights.get_mut(&String::from(name))
+    }
+
+}
